6ceef0f9c9235c107038b805d82b4e69a85a7b87
Makhabat <maksatbekova.m@gmail.com>
2021-02-20 01:00:52 -0800

    madlibs project is done

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+adj = input("Adjective: ")
+verb = input("Verb: ")
+food = input("Favorite food: ")
+madlibs = f"Today I had an {adj} day. When I have a free time, I like to  {verb}. Mostly I eat {food}."
+
+print(madlibs)
