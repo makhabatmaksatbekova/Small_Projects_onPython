fd64490cbfb25aaa4c15f065b319804684aca9a5
Makhabat <maksatbekova.m@gmail.com>
2021-02-20 13:33:48 -0800

    guessing game is done

---
+++ guessing_game.py
@@ -0,0 +1,15 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"Give me a number between 1 and {x}: "))
+        if guess < random_number:
+            print("Sorry, try again. Your number is too low.")
+        elif guess > random_number:
+            print("Try again. Your number is too high.")
+    print(f"You have got the correct number {random_number}!")
+
+   
+guess(10)
